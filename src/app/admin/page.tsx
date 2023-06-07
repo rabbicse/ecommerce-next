f15d8e411cadbf098c7d5965c1caa6468d70443b
Mehmet <rabbi.cse.sust.bd@gmail.com>
2023-06-08 00:38:04 +0600

    Update frontend template

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,25 @@
+import AdminLayout from '@/app/admin/layout';
+import { ReactElement } from 'react';
+
+
+export default function AdminPage() {
+  return (
+    <>
+    <div>
+        Admin
+        </div>
+        </>
+  )
+}
+
+// AdminPage.getLayout = function getLayout(page: ReactElement) {
+//     return (
+//         <>
+//       <AdminLayout>
+//         {page}
+//       </AdminLayout>
+//       </>
+//     );
+//   };
+
+//   export default AdminPage;
