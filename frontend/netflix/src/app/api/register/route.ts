d30109514ae178cdda797b18f23709de5dc1716e
MD. Mehedi Hasan Rabbi <rabbi.se@gmail.com>
2023-07-26 16:35:34 +0600

    update netflix frontend with mongodb integration

---
+++ frontend/netflix/src/app/api/register/route.ts
@@ -0,0 +1,40 @@
+import bcrypt from 'bcrypt';
+import prismadb from '@/lib/prismadb';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request, res: Response) {
+    try {
+        const reqBody = await req.json();
+        const { email, name, password } = reqBody;
+
+        // const existingUser = await prismadb.user.findUnique({
+        //     where: {
+        //         email
+        //     }
+        // })
+
+        // if (existingUser) {
+        //     return NextResponse.json({ error: 'Email taken' }, {
+        //         status: 400
+        //     });
+        // }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = await prismadb.user.create({
+            data: {
+                email,
+                name,
+                hashedPassword,
+                image: '',
+                emailVerified: new Date(),
+            }
+        })
+
+        return NextResponse.json(user);
+    } catch (error) {
+        return NextResponse.json({ message: `Something went wrong: ${error}` }, {
+            status: 500
+        });
+    }
+}
--- frontend/netflix/src/app/auth/page.tsx
+++ frontend/netflix/src/app/auth/page.tsx
@@ -1,8 +1,17 @@
 "use client"
+import axios from "axios"
 import { useCallback, useState } from "react";
+import { NextPageContext } from 'next';
+import { getSession, signIn } from 'next-auth/react';
+// import { useRouter } from 'next/router';
+// import { FcGoogle } from 'react-icons/fc';
+// import { FaGithub } from 'react-icons/fa';
+
 import Input from "@/components/Input";
 
 const Auth = () => {
+    // const router = useRouter();
+
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -12,6 +21,35 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
     }, []);
+
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+
+            // router.push('/profiles');
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, password]);
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            });
+
+            // login();
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, name, password, login]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -27,29 +65,39 @@ const Auth = () => {
                         <div className="flex flex-col gap-4">
                             {variant === 'register' && (
                                 <Input
-                                    label="Username"
-                                    onChange={(ev: any) => setName(ev.target.value)}
                                     id="name"
-                                    value={name} />
+                                    type="text"
+                                    label="Username"
+                                    value={name}
+                                    onChange={(e: any) => setName(e.target.value)}
+                                />
                             )}
                             <Input
-                                label="Email"
                                 id="email"
                                 type="email"
+                                label="Email address or phone number"
                                 value={email}
-                                onChange={(ev: any) => { setEmail(ev.target.value) }}
+                                onChange={(e: any) => setEmail(e.target.value)}
                             />
                             <Input
-                                label="Password"
-                                id="password"
                                 type="password"
+                                id="password"
+                                label="Password"
                                 value={password}
-                                onChange={(ev: any) => setPassword(ev.target.value)}
+                                onChange={(e: any) => setPassword(e.target.value)}
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                            Login
+                        <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
+                        {/* <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FcGoogle size={32} />
+                            </div>
+                            <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                <FaGithub size={32} />
+                            </div>
+                        </div> */}
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
