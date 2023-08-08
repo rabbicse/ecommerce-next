92222f3bb46baa1f5a9ac77197b4927f914cab2d
Mehmet <rabbi.cse.sust.bd@gmail.com>
2023-08-08 20:27:59 +0600

    Update ecommerce admin auth UI

---
+++ frontend/ecommerce-admin/src/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className='p-4'>
+      Hello Admin
+      <Button size="default" variant="destructive">Click Me</Button>
+    </div>
+  )
+}
